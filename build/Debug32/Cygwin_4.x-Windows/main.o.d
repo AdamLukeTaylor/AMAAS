d10b7c0b397cb129ea65645a4c89aa3a72754191
tayloral <tayloral@tcd.ie>
2014-11-17 10:05:11 +0000

    amass in right place

---
+++ build/Debug32/Cygwin_4.x-Windows/main.o.d
@@ -0,0 +1,56 @@
+build/Debug32/Cygwin_4.x-Windows/main.o: main.cpp CartPole.h \
+ /cygdrive/C/Users/Adam/Documents/NetBeansProjects/AMAAS/DWL/include/DWLAgent.h \
+ /cygdrive/C/Users/Adam/Documents/NetBeansProjects/AMAAS/DWL/include/Policy.h \
+ /cygdrive/C/Users/Adam/Documents/NetBeansProjects/AMAAS/DWL/include/Reward.h \
+ /cygdrive/C/Users/Adam/Documents/NetBeansProjects/AMAAS/DWL/include/WLearningProcess.h \
+ /cygdrive/C/Users/Adam/Documents/NetBeansProjects/AMAAS/DWL/include/WTable.h \
+ /cygdrive/C/Users/Adam/Documents/NetBeansProjects/AMAAS/DWL/include/QTable.h \
+ /cygdrive/C/Users/Adam/Documents/NetBeansProjects/AMAAS/DWL/include/Boltzmann.h \
+ /cygdrive/C/Users/Adam/Documents/NetBeansProjects/AMAAS/DWL/include/ActionSelection.h \
+ /cygdrive/C/Users/Adam/Documents/NetBeansProjects/AMAAS/DWL/include/EGready.h \
+ /cygdrive/C/Users/Adam/Documents/NetBeansProjects/AMAAS/DWL/include/TaylorSeriesSelection.h \
+ /cygdrive/C/Users/Adam/Documents/NetBeansProjects/AMAAS/DWL/include/CollaborationProcess.h \
+ /cygdrive/C/Users/Adam/Documents/NetBeansProjects/AMAAS/DWL/include/TransferMapping.h \
+ CartPoleAgent.h DWL/include/DWLAgent.h MtCarAgent.h Constants.h MtCar.h \
+ /cygdrive/C/Program\ Files\ (x86)/Visual\ Leak\ Detector/src/vld.h \
+ /cygdrive/C/Program\ Files\ (x86)/Visual\ Leak\ Detector/src/vld_def.h
+
+CartPole.h:
+
+/cygdrive/C/Users/Adam/Documents/NetBeansProjects/AMAAS/DWL/include/DWLAgent.h:
+
+/cygdrive/C/Users/Adam/Documents/NetBeansProjects/AMAAS/DWL/include/Policy.h:
+
+/cygdrive/C/Users/Adam/Documents/NetBeansProjects/AMAAS/DWL/include/Reward.h:
+
+/cygdrive/C/Users/Adam/Documents/NetBeansProjects/AMAAS/DWL/include/WLearningProcess.h:
+
+/cygdrive/C/Users/Adam/Documents/NetBeansProjects/AMAAS/DWL/include/WTable.h:
+
+/cygdrive/C/Users/Adam/Documents/NetBeansProjects/AMAAS/DWL/include/QTable.h:
+
+/cygdrive/C/Users/Adam/Documents/NetBeansProjects/AMAAS/DWL/include/Boltzmann.h:
+
+/cygdrive/C/Users/Adam/Documents/NetBeansProjects/AMAAS/DWL/include/ActionSelection.h:
+
+/cygdrive/C/Users/Adam/Documents/NetBeansProjects/AMAAS/DWL/include/EGready.h:
+
+/cygdrive/C/Users/Adam/Documents/NetBeansProjects/AMAAS/DWL/include/TaylorSeriesSelection.h:
+
+/cygdrive/C/Users/Adam/Documents/NetBeansProjects/AMAAS/DWL/include/CollaborationProcess.h:
+
+/cygdrive/C/Users/Adam/Documents/NetBeansProjects/AMAAS/DWL/include/TransferMapping.h:
+
+CartPoleAgent.h:
+
+DWL/include/DWLAgent.h:
+
+MtCarAgent.h:
+
+Constants.h:
+
+MtCar.h:
+
+/cygdrive/C/Program\ Files\ (x86)/Visual\ Leak\ Detector/src/vld.h:
+
+/cygdrive/C/Program\ Files\ (x86)/Visual\ Leak\ Detector/src/vld_def.h:
---
+++ nbproject/Makefile-debug64.mk
@@ -0,0 +1,198 @@
+#
+# Generated Makefile - do not edit!
+#
+# Edit the Makefile in the project folder instead (../Makefile). Each target
+# has a -pre and a -post target defined where you can add customized code.
+#
+# This makefile implements configuration specific macros and targets.
+
+
+# Environment
+MKDIR=mkdir
+CP=cp
+GREP=grep
+NM=nm
+CCADMIN=CCadmin
+RANLIB=ranlib
+CC=gcc
+CCC=g++
+CXX=g++
+FC=gfortran
+AS=as
+
+# Macros
+CND_PLATFORM=Cygwin_4.x_64-Windows
+CND_DLIB_EXT=dll
+CND_CONF=debug64
+CND_DISTDIR=dist
+CND_BUILDDIR=build
+
+# Include project Makefile
+include Makefile
+
+# Object Directory
+OBJECTDIR=${CND_BUILDDIR}/${CND_CONF}/${CND_PLATFORM}
+
+# Object Files
+OBJECTFILES= \
+	${OBJECTDIR}/_ext/875747664/CartPole.o \
+	${OBJECTDIR}/_ext/875747664/CartPoleAgent.o \
+	${OBJECTDIR}/_ext/2123244687/ActionSelection.o \
+	${OBJECTDIR}/_ext/2123244687/Boltzmann.o \
+	${OBJECTDIR}/_ext/2123244687/CollaborationProcess.o \
+	${OBJECTDIR}/_ext/2123244687/DWLAgent.o \
+	${OBJECTDIR}/_ext/2123244687/EGready.o \
+	${OBJECTDIR}/_ext/2123244687/NeighbourReward.o \
+	${OBJECTDIR}/_ext/2123244687/Policy.o \
+	${OBJECTDIR}/_ext/2123244687/QTable.o \
+	${OBJECTDIR}/_ext/2123244687/Reward.o \
+	${OBJECTDIR}/_ext/2123244687/TaylorSeriesSelection.o \
+	${OBJECTDIR}/_ext/2123244687/TransferMapping.o \
+	${OBJECTDIR}/_ext/2123244687/WLearningProcess.o \
+	${OBJECTDIR}/_ext/2123244687/WTable.o \
+	${OBJECTDIR}/_ext/875747664/MtCar.o \
+	${OBJECTDIR}/_ext/875747664/MtCarAgent.o \
+	${OBJECTDIR}/_ext/875747664/RewardCartPole.o \
+	${OBJECTDIR}/_ext/875747664/RewardMtCar.o \
+	${OBJECTDIR}/main.o
+
+
+# C Compiler Flags
+CFLAGS=-m64
+
+# CC Compiler Flags
+CCFLAGS=-m64
+CXXFLAGS=-m64
+
+# Fortran Compiler Flags
+FFLAGS=-m64
+
+# Assembler Flags
+ASFLAGS=--64
+
+# Link Libraries and Options
+LDLIBSOPTIONS=
+
+# Build Targets
+.build-conf: ${BUILD_SUBPROJECTS}
+	"${MAKE}"  -f nbproject/Makefile-${CND_CONF}.mk ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/amaas.exe
+
+${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/amaas.exe: ${OBJECTFILES}
+	${MKDIR} -p ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}
+	${LINK.cc} -o ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/amaas ${OBJECTFILES} ${LDLIBSOPTIONS}
+
+${OBJECTDIR}/_ext/875747664/CartPole.o: /cygdrive/C/Users/Adam/Documents/NetBeansProjects/AMAAS/CartPole.cpp 
+	${MKDIR} -p ${OBJECTDIR}/_ext/875747664
+	${RM} "$@.d"
+	$(COMPILE.cc) -g -I/cygdrive/C/Users/Adam/Documents/NetBeansProjects/AMAAS/DWL/include -I/cygdrive/C/Users/Adam/Documents/NetBeansProjects/AMAAS/MtCar -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/_ext/875747664/CartPole.o /cygdrive/C/Users/Adam/Documents/NetBeansProjects/AMAAS/CartPole.cpp
+
+${OBJECTDIR}/_ext/875747664/CartPoleAgent.o: /cygdrive/C/Users/Adam/Documents/NetBeansProjects/AMAAS/CartPoleAgent.cpp 
+	${MKDIR} -p ${OBJECTDIR}/_ext/875747664
+	${RM} "$@.d"
+	$(COMPILE.cc) -g -I/cygdrive/C/Users/Adam/Documents/NetBeansProjects/AMAAS/DWL/include -I/cygdrive/C/Users/Adam/Documents/NetBeansProjects/AMAAS/MtCar -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/_ext/875747664/CartPoleAgent.o /cygdrive/C/Users/Adam/Documents/NetBeansProjects/AMAAS/CartPoleAgent.cpp
+
+${OBJECTDIR}/_ext/2123244687/ActionSelection.o: /cygdrive/C/Users/Adam/Documents/NetBeansProjects/AMAAS/DWL/src/ActionSelection.cpp 
+	${MKDIR} -p ${OBJECTDIR}/_ext/2123244687
+	${RM} "$@.d"
+	$(COMPILE.cc) -g -I/cygdrive/C/Users/Adam/Documents/NetBeansProjects/AMAAS/DWL/include -I/cygdrive/C/Users/Adam/Documents/NetBeansProjects/AMAAS/MtCar -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/_ext/2123244687/ActionSelection.o /cygdrive/C/Users/Adam/Documents/NetBeansProjects/AMAAS/DWL/src/ActionSelection.cpp
+
+${OBJECTDIR}/_ext/2123244687/Boltzmann.o: /cygdrive/C/Users/Adam/Documents/NetBeansProjects/AMAAS/DWL/src/Boltzmann.cpp 
+	${MKDIR} -p ${OBJECTDIR}/_ext/2123244687
+	${RM} "$@.d"
+	$(COMPILE.cc) -g -I/cygdrive/C/Users/Adam/Documents/NetBeansProjects/AMAAS/DWL/include -I/cygdrive/C/Users/Adam/Documents/NetBeansProjects/AMAAS/MtCar -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/_ext/2123244687/Boltzmann.o /cygdrive/C/Users/Adam/Documents/NetBeansProjects/AMAAS/DWL/src/Boltzmann.cpp
+
+${OBJECTDIR}/_ext/2123244687/CollaborationProcess.o: /cygdrive/C/Users/Adam/Documents/NetBeansProjects/AMAAS/DWL/src/CollaborationProcess.cpp 
+	${MKDIR} -p ${OBJECTDIR}/_ext/2123244687
+	${RM} "$@.d"
+	$(COMPILE.cc) -g -I/cygdrive/C/Users/Adam/Documents/NetBeansProjects/AMAAS/DWL/include -I/cygdrive/C/Users/Adam/Documents/NetBeansProjects/AMAAS/MtCar -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/_ext/2123244687/CollaborationProcess.o /cygdrive/C/Users/Adam/Documents/NetBeansProjects/AMAAS/DWL/src/CollaborationProcess.cpp
+
+${OBJECTDIR}/_ext/2123244687/DWLAgent.o: /cygdrive/C/Users/Adam/Documents/NetBeansProjects/AMAAS/DWL/src/DWLAgent.cpp 
+	${MKDIR} -p ${OBJECTDIR}/_ext/2123244687
+	${RM} "$@.d"
+	$(COMPILE.cc) -g -I/cygdrive/C/Users/Adam/Documents/NetBeansProjects/AMAAS/DWL/include -I/cygdrive/C/Users/Adam/Documents/NetBeansProjects/AMAAS/MtCar -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/_ext/2123244687/DWLAgent.o /cygdrive/C/Users/Adam/Documents/NetBeansProjects/AMAAS/DWL/src/DWLAgent.cpp
+
+${OBJECTDIR}/_ext/2123244687/EGready.o: /cygdrive/C/Users/Adam/Documents/NetBeansProjects/AMAAS/DWL/src/EGready.cpp 
+	${MKDIR} -p ${OBJECTDIR}/_ext/2123244687
+	${RM} "$@.d"
+	$(COMPILE.cc) -g -I/cygdrive/C/Users/Adam/Documents/NetBeansProjects/AMAAS/DWL/include -I/cygdrive/C/Users/Adam/Documents/NetBeansProjects/AMAAS/MtCar -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/_ext/2123244687/EGready.o /cygdrive/C/Users/Adam/Documents/NetBeansProjects/AMAAS/DWL/src/EGready.cpp
+
+${OBJECTDIR}/_ext/2123244687/NeighbourReward.o: /cygdrive/C/Users/Adam/Documents/NetBeansProjects/AMAAS/DWL/src/NeighbourReward.cpp 
+	${MKDIR} -p ${OBJECTDIR}/_ext/2123244687
+	${RM} "$@.d"
+	$(COMPILE.cc) -g -I/cygdrive/C/Users/Adam/Documents/NetBeansProjects/AMAAS/DWL/include -I/cygdrive/C/Users/Adam/Documents/NetBeansProjects/AMAAS/MtCar -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/_ext/2123244687/NeighbourReward.o /cygdrive/C/Users/Adam/Documents/NetBeansProjects/AMAAS/DWL/src/NeighbourReward.cpp
+
+${OBJECTDIR}/_ext/2123244687/Policy.o: /cygdrive/C/Users/Adam/Documents/NetBeansProjects/AMAAS/DWL/src/Policy.cpp 
+	${MKDIR} -p ${OBJECTDIR}/_ext/2123244687
+	${RM} "$@.d"
+	$(COMPILE.cc) -g -I/cygdrive/C/Users/Adam/Documents/NetBeansProjects/AMAAS/DWL/include -I/cygdrive/C/Users/Adam/Documents/NetBeansProjects/AMAAS/MtCar -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/_ext/2123244687/Policy.o /cygdrive/C/Users/Adam/Documents/NetBeansProjects/AMAAS/DWL/src/Policy.cpp
+
+${OBJECTDIR}/_ext/2123244687/QTable.o: /cygdrive/C/Users/Adam/Documents/NetBeansProjects/AMAAS/DWL/src/QTable.cpp 
+	${MKDIR} -p ${OBJECTDIR}/_ext/2123244687
+	${RM} "$@.d"
+	$(COMPILE.cc) -g -I/cygdrive/C/Users/Adam/Documents/NetBeansProjects/AMAAS/DWL/include -I/cygdrive/C/Users/Adam/Documents/NetBeansProjects/AMAAS/MtCar -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/_ext/2123244687/QTable.o /cygdrive/C/Users/Adam/Documents/NetBeansProjects/AMAAS/DWL/src/QTable.cpp
+
+${OBJECTDIR}/_ext/2123244687/Reward.o: /cygdrive/C/Users/Adam/Documents/NetBeansProjects/AMAAS/DWL/src/Reward.cpp 
+	${MKDIR} -p ${OBJECTDIR}/_ext/2123244687
+	${RM} "$@.d"
+	$(COMPILE.cc) -g -I/cygdrive/C/Users/Adam/Documents/NetBeansProjects/AMAAS/DWL/include -I/cygdrive/C/Users/Adam/Documents/NetBeansProjects/AMAAS/MtCar -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/_ext/2123244687/Reward.o /cygdrive/C/Users/Adam/Documents/NetBeansProjects/AMAAS/DWL/src/Reward.cpp
+
+${OBJECTDIR}/_ext/2123244687/TaylorSeriesSelection.o: /cygdrive/C/Users/Adam/Documents/NetBeansProjects/AMAAS/DWL/src/TaylorSeriesSelection.cpp 
+	${MKDIR} -p ${OBJECTDIR}/_ext/2123244687
+	${RM} "$@.d"
+	$(COMPILE.cc) -g -I/cygdrive/C/Users/Adam/Documents/NetBeansProjects/AMAAS/DWL/include -I/cygdrive/C/Users/Adam/Documents/NetBeansProjects/AMAAS/MtCar -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/_ext/2123244687/TaylorSeriesSelection.o /cygdrive/C/Users/Adam/Documents/NetBeansProjects/AMAAS/DWL/src/TaylorSeriesSelection.cpp
+
+${OBJECTDIR}/_ext/2123244687/TransferMapping.o: /cygdrive/C/Users/Adam/Documents/NetBeansProjects/AMAAS/DWL/src/TransferMapping.cpp 
+	${MKDIR} -p ${OBJECTDIR}/_ext/2123244687
+	${RM} "$@.d"
+	$(COMPILE.cc) -g -I/cygdrive/C/Users/Adam/Documents/NetBeansProjects/AMAAS/DWL/include -I/cygdrive/C/Users/Adam/Documents/NetBeansProjects/AMAAS/MtCar -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/_ext/2123244687/TransferMapping.o /cygdrive/C/Users/Adam/Documents/NetBeansProjects/AMAAS/DWL/src/TransferMapping.cpp
+
+${OBJECTDIR}/_ext/2123244687/WLearningProcess.o: /cygdrive/C/Users/Adam/Documents/NetBeansProjects/AMAAS/DWL/src/WLearningProcess.cpp 
+	${MKDIR} -p ${OBJECTDIR}/_ext/2123244687
+	${RM} "$@.d"
+	$(COMPILE.cc) -g -I/cygdrive/C/Users/Adam/Documents/NetBeansProjects/AMAAS/DWL/include -I/cygdrive/C/Users/Adam/Documents/NetBeansProjects/AMAAS/MtCar -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/_ext/2123244687/WLearningProcess.o /cygdrive/C/Users/Adam/Documents/NetBeansProjects/AMAAS/DWL/src/WLearningProcess.cpp
+
+${OBJECTDIR}/_ext/2123244687/WTable.o: /cygdrive/C/Users/Adam/Documents/NetBeansProjects/AMAAS/DWL/src/WTable.cpp 
+	${MKDIR} -p ${OBJECTDIR}/_ext/2123244687
+	${RM} "$@.d"
+	$(COMPILE.cc) -g -I/cygdrive/C/Users/Adam/Documents/NetBeansProjects/AMAAS/DWL/include -I/cygdrive/C/Users/Adam/Documents/NetBeansProjects/AMAAS/MtCar -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/_ext/2123244687/WTable.o /cygdrive/C/Users/Adam/Documents/NetBeansProjects/AMAAS/DWL/src/WTable.cpp
+
+${OBJECTDIR}/_ext/875747664/MtCar.o: /cygdrive/C/Users/Adam/Documents/NetBeansProjects/AMAAS/MtCar.cpp 
+	${MKDIR} -p ${OBJECTDIR}/_ext/875747664
+	${RM} "$@.d"
+	$(COMPILE.cc) -g -I/cygdrive/C/Users/Adam/Documents/NetBeansProjects/AMAAS/DWL/include -I/cygdrive/C/Users/Adam/Documents/NetBeansProjects/AMAAS/MtCar -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/_ext/875747664/MtCar.o /cygdrive/C/Users/Adam/Documents/NetBeansProjects/AMAAS/MtCar.cpp
+
+${OBJECTDIR}/_ext/875747664/MtCarAgent.o: /cygdrive/C/Users/Adam/Documents/NetBeansProjects/AMAAS/MtCarAgent.cpp 
+	${MKDIR} -p ${OBJECTDIR}/_ext/875747664
+	${RM} "$@.d"
+	$(COMPILE.cc) -g -I/cygdrive/C/Users/Adam/Documents/NetBeansProjects/AMAAS/DWL/include -I/cygdrive/C/Users/Adam/Documents/NetBeansProjects/AMAAS/MtCar -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/_ext/875747664/MtCarAgent.o /cygdrive/C/Users/Adam/Documents/NetBeansProjects/AMAAS/MtCarAgent.cpp
+
+${OBJECTDIR}/_ext/875747664/RewardCartPole.o: /cygdrive/C/Users/Adam/Documents/NetBeansProjects/AMAAS/RewardCartPole.cpp 
+	${MKDIR} -p ${OBJECTDIR}/_ext/875747664
+	${RM} "$@.d"
+	$(COMPILE.cc) -g -I/cygdrive/C/Users/Adam/Documents/NetBeansProjects/AMAAS/DWL/include -I/cygdrive/C/Users/Adam/Documents/NetBeansProjects/AMAAS/MtCar -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/_ext/875747664/RewardCartPole.o /cygdrive/C/Users/Adam/Documents/NetBeansProjects/AMAAS/RewardCartPole.cpp
+
+${OBJECTDIR}/_ext/875747664/RewardMtCar.o: /cygdrive/C/Users/Adam/Documents/NetBeansProjects/AMAAS/RewardMtCar.cpp 
+	${MKDIR} -p ${OBJECTDIR}/_ext/875747664
+	${RM} "$@.d"
+	$(COMPILE.cc) -g -I/cygdrive/C/Users/Adam/Documents/NetBeansProjects/AMAAS/DWL/include -I/cygdrive/C/Users/Adam/Documents/NetBeansProjects/AMAAS/MtCar -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/_ext/875747664/RewardMtCar.o /cygdrive/C/Users/Adam/Documents/NetBeansProjects/AMAAS/RewardMtCar.cpp
+
+${OBJECTDIR}/main.o: main.cpp 
+	${MKDIR} -p ${OBJECTDIR}
+	${RM} "$@.d"
+	$(COMPILE.cc) -g -I/cygdrive/C/Users/Adam/Documents/NetBeansProjects/AMAAS/DWL/include -I/cygdrive/C/Users/Adam/Documents/NetBeansProjects/AMAAS/MtCar -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/main.o main.cpp
+
+# Subprojects
+.build-subprojects:
+
+# Clean Targets
+.clean-conf: ${CLEAN_SUBPROJECTS}
+	${RM} -r ${CND_BUILDDIR}/${CND_CONF}
+	${RM} ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/amaas.exe
+
+# Subprojects
+.clean-subprojects:
+
+# Enable dependency checking
+.dep.inc: .depcheck-impl
+
+include .dep.inc
